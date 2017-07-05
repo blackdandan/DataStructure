6d941ac722c5f8347b5ec406da6d71286057cd8d
blackdandan <784818984@qq.com>
2017-07-05 22:57:39 +0800

    [新增][Stack]用Java实现Stack

--- app/src/main/java/com/example/blackdandan/datastructure/MainActivity.java
+++ app/src/main/java/com/example/blackdandan/datastructure/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 
 import com.example.blackdandan.datastructure.somedatastructure.list.MyList;
+import com.example.blackdandan.datastructure.somedatastructure.list.MyStack;
 
 public class MainActivity extends Activity {
 
@@ -11,18 +12,28 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        /**
+//         * 测试 {@link com.example.blackdandan.datastructure.somedatastructure.list.MyList}
+//         */
+//        MyList<String> list = new MyList<>();
+//        for (int i = 0 ;i<10;i++){
+//            list.add(""+i);
+//        }
+//        System.out.println("do====List.size:"+list.size());
+//
+//        System.out.println("do====List.isEmpty"+list.isEmpty());
+//        for (int i = 0 ;i<list.size();i++){
+//            System.out.println("do====List.get"+i+":"+list.get(i));
+//        }
         /**
-         * 测试 {@link com.example.blackdandan.datastructure.somedatastructure.list.MyList}
+         * 测试 {@link com.example.blackdandan.datastructure.somedatastructure.list.MyStack}
          */
-        MyList<String> list = new MyList<>();
-        for (int i = 0 ;i<10;i++){
-            list.add(""+i);
+        MyStack<Integer> myStack = new MyStack<>();
+        for (int i = 0;i<10;i++){
+            myStack.push(i);
         }
-        System.out.println("do====List.size:"+list.size());
-
-        System.out.println("do====List.isEmpty"+list.isEmpty());
-        for (int i = 0 ;i<list.size();i++){
-            System.out.println("do====List.get"+i+":"+list.get(i));
+        for (int i = 0;i<10;i++){
+            System.out.println("do===="+i+"=="+myStack.pop());
         }
     }
 }
---
+++ app/src/main/java/com/example/blackdandan/datastructure/somedatastructure/list/MyStack.java
@@ -0,0 +1,41 @@
+package com.example.blackdandan.datastructure.somedatastructure.list;
+
+/**
+ * description :
+ * Created by blackdandan on 2017/7/5.
+ */
+
+public class MyStack<T> {
+    private Node<T> topNode;
+    private int size = 0;
+    private Node<T> base;
+    public MyStack(){
+        this.size = 0;
+        this.base = new Node<>(null,null);
+        this.topNode = new Node<>(null,base);
+
+    }
+    public void push(T data){
+        Node<T> item = new Node<>(data,topNode);
+        topNode = item;
+        size ++;
+    }
+    public boolean isEmpty(){
+        return size == 0;
+    }
+    public T pop(){
+        if (isEmpty())return null;
+        T topData = topNode.data;
+        topNode = topNode.base;
+        size --;
+        return topData;
+    }
+    private class Node<T>{
+        public T data;
+        public Node<T> base;
+        public Node(T data,Node<T> base){
+            this.data = data;
+            this.base = base;
+        }
+    }
+}
